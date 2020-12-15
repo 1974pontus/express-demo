dd2db20d38c62b224f90d9d1a9c1003eae3f8bdd
Pontus Tahir <pontus.tahir@gmail.com>
2020-12-15 12:20:09 +0100

    refactoring

--- index.js
+++ index.js
@@ -16,23 +16,21 @@ app.get('/api/guitars', (req, res) => {
 })
 
 app.get('/api/guitars/:id', (req, res) => {
-    // let data = fs.readFileSync('./guitars.json')
-    // let guitars = JSON.parse(data)
     const guitar = guitars.find(g => g.id === parseInt(req.params.id))
     if (!guitar) res.status(404).send('The guitar with the given ID was not found:(')
     res.send(JSON.stringify(guitar))
 })
 
 app.post('/api/guitars', (req, res) => {
-    let idIndex = 5
-    // let data = fs.readFileSync('./guitars.json')
-    // let guitars = JSON.parse(data)
+    let idIndex = new Date()
     if (req.body.year > 1999) {
         res.status(400).send('Guitars in vintage Wishlist must be older than that!')
         return
     }
-    console.log("body", req.body)
-    const guitar = { id: idIndex++, ...req.body}
+    parseInt(req.body.year);
+    // console.log("body", req.body)
+    console.log("year", req.body.brand)
+    const guitar = { id: idIndex, ...req.body}
     guitars.push(guitar)
     res.status(201)
     fs.writeFileSync('./guitars.json', JSON.stringify(guitars, null, 2));
